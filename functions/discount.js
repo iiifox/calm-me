869541e96f5a29449e8a682a3bad6d0a39974b76
iiifox <86429225+iiifox@users.noreply.github.com>
2025-08-30 23:24:09 +0800

    Update discount.js

--- functions/discount.js
+++ functions/discount.js
@@ -149,7 +149,7 @@ export async function onRequest(context) {
     }
 
     const qz = parseQz(qzLines);
-    const gbo = await parseGbo(gboLines, request);
+    const gbo = parseGbo(gboLines, request);
 
     const out = {yesterdayPage, date, qz, gbo};
     return new Response(JSON.stringify(out, null, 2), {
