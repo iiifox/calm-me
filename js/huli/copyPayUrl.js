0a19d7338ca40f1c0a6540d2e9fee600fb0c2172
iiifox <86429225+iiifox@users.noreply.github.com>
2025-09-16 21:35:26 +0800

    Update copyPayUrl.js

--- js/huli/copyPayUrl.js
+++ js/huli/copyPayUrl.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         提取狐狸代付链接
-// @namespace    http://tampermonkey.net/
+// @namespace    https://iiifox.me/
 // @version      0.3
 // @description  打开狐狸直冲链接时，捕获代付链接并提供按钮点击复制功能，支持 http 环境，带 Toast 提示（右下角），避免假复制问题。
 // @author       iiifox
 // @match        *://104.143.42.32/*
 // @grant        none
 // @run-at       document-start
-// @updateURL    https://iiifox.me/js/copyHuliUrl.js
-// @downloadURL  https://iiifox.me/js/copyHuliUrl.js
+// @updateURL    https://iiifox.me/js/huli/copyPayUrl.js
+// @downloadURL  https://iiifox.me/js/huli/copyPayUrl.js
 // ==/UserScript==
 
 (function() {
