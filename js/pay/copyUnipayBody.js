ec623e49f653ce0d76cf174b0ad086727793e842
iiifox <86429225+iiifox@users.noreply.github.com>
2025-09-21 12:10:15 +0800

    Update and rename js/copyUnipayBody.js to js/pay/copyUnipayBody.js

--- js/pay/copyUnipayBody.js
+++ js/pay/copyUnipayBody.js
@@ -7,7 +7,7 @@
 // @match        *://pay.qq.com/*
 // @grant        none
 // @run-at       document-start
-// @updateURL    https://iiifox.me/js/copyUnipayBody.js
+// @updateURL    https://iiifox.me/js/pay/copyUnipayBody.js
 // ==/UserScript==
 
 (function () {
