fdfc18aede5307ed3f217272f43a191094f7e5bf
iiifox <86429225+iiifox@users.noreply.github.com>
2025-09-26 23:22:06 +0800

    Update autoTransmission.js

--- js/pay/autoTransmission.js
+++ js/pay/autoTransmission.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动传码
 // @namespace    https://iiifox.me/
-// @version      1.0
+// @version      2.0
 // @description  自动传码到饭票（需填写url与次数）
 // @author       iiifox
 // @match        *://pay.qq.com/*
@@ -120,5 +120,6 @@
         });
     };
 })();
+
 
 
