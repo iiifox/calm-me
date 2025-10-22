1f47f04f8555502306f4efe2cdcbb82c959da29e
iiifox <86429225+iiifox@users.noreply.github.com>
2025-10-23 04:41:40 +0800

    Update chuanma.js

--- assets/huli/chuanma.js
+++ assets/huli/chuanma.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         狐狸自动传码
 // @namespace    https://iiifox.me/
-// @version      0.0.2
+// @version      0.0.3
 // @description  狐狸自动传码，此为初版，非正式版。功能待优化
 // @author       iiifox
 // @match        *://pay.qq.com/*
@@ -9,6 +9,8 @@
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
 // @run-at       document-start
+// @updateURL    https://iiifox.me/assets/huli/chuanma.js
+// @downloadURL  https://iiifox.me/assets/huli/chuanma.js
 // @connect      081w5a8cim.top
 // @connect      8w0m6rjg3l.top
 // ==/UserScript==
