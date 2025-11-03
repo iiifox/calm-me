d77d9ef19d0f86c8c69a205e97e53720a5f5b88e
iiifox <86429225+iiifox@users.noreply.github.com>
2025-11-04 00:15:05 +0800

    Update discount.js

--- functions/api/discount.js
+++ functions/api/discount.js
@@ -23,7 +23,7 @@ function parseXd(lines, profit) {
     const timeOrder = [];
     let currentTimeKey = '';
 
-    const specialMap = {VB: "VB微信10起", VC: "VC微信50", VD: "VD100", VE: "VE200", "H": "渠道H（低价）88.5"};
+    const specialMap = {VB: "VB微信10起", VC: "VC微信50", VD: "VD100", VE: "VE200", "H": "渠道H（低价）"};
 
     const channelsFirstIndex = new Map();
 
