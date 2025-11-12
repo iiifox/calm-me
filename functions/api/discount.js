1a40ef9666cb0fc595f0476b45bd5819a87257ff
iiifox <86429225+iiifox@users.noreply.github.com>
2025-11-12 23:55:18 +0800

    Update discount.js

--- functions/api/discount.js
+++ functions/api/discount.js
@@ -90,7 +90,7 @@ function parseXy(lines, profit, xd) {
     const timeOrder = [];
     let currentTimeKey = '';
 
-    const specialMap = {"直拉": "钱包直拉", "单端": "微信单端", "小额": "微信小额", "双端": "微信通额", "扫码": "微信扫码"};
+    const specialMap = {"直拉": "钱包直拉", "单端": "微信单端", "通额": "微信通额", "小额": "微信小额", "点额": "微信点额", "扫码": "微信扫码"};
     const channelsFirstIndex = new Map();
 
     const xdToXy = {"渠道A": "普通", "渠道B": "加速", "渠道C": "超速", "渠道D": "极速", "渠道E": "秒拉", "渠道F": "钱包直拉", "渠道TA": "怪额", "渠道TB": "超怪"}
