eb0bf760512c2d0b2415a181723b29a355a7593e
iiifox <86429225+iiifox@users.noreply.github.com>
2025-11-18 13:51:26 +0800

    Update pizi1.js

--- assets/giraffe/login/pizi1.js
+++ assets/giraffe/login/pizi1.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         长颈鹿pizi1注入
 // @namespace    https://iiifox.me/
-// @version      1.0.0
+// @version      1.1.0
 // @description  狐狸登录页面注入pizi1账号密码谷歌验证码
 // @author       iiifox
-// @match        http://121.43.147.96:8369/WebLogin.aspx
+// @match        http://121.43.147.96:8369/weblogin.aspx
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_registerMenuCommand
@@ -18,7 +18,7 @@
 
     // 配置参数
     const TARGET_HOST = '121.43.147.96:8369';
-    const TARGET_PATH = '/WebLogin.aspx';
+    const TARGET_PATH = '/weblogin.aspx';
     const TOTP_API_URL = 'https://iiifox.me/totp';
 
     function isAllowedPath() {
