68035fc8906c57e78c6e8f5a6508658f5e295a71
iiifox <86429225+iiifox@users.noreply.github.com>
2025-11-25 17:13:22 +0800

    Update copyPayUrl.js

--- script/giraffe/copyPayUrl.js
+++ script/giraffe/copyPayUrl.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         提取长颈鹿代付链接
-// @namespace    https://iiifox.me/
+// @namespace    https://luei.me/
 // @version      1.0.0
 // @description  打开长颈鹿直冲链接时，捕获代付链接并提供按钮点击复制功能
 // @author       iiifox
 // @match        *://38.47.218.60/*
 // @grant        none
 // @run-at       document-start
-// @updateURL    https://iiifox.me/script/giraffe/copyPayUrl.js
+// @updateURL    https://luei.me/script/giraffe/copyPayUrl.js
 // ==/UserScript==
 
 (function() {
